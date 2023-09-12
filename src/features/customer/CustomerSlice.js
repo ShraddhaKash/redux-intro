d1dffb5104d6e5138745fcc8184d59956d111df5
Shraddha Kashyap <kashyap.shraddha@tftus.com>
2023-09-12 17:56:31 +0530

    Commit

---
+++ src/features/customer/CustomerSlice.js
@@ -0,0 +1,67 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  fullName: "",
+  nationalID: "",
+  createAT: "",
+};
+
+const customerSlice = createSlice({
+  name: "customer",
+  initialState,
+  reducers: {
+    createCustomer: {
+      prepare(fullName, nationalID) {
+        return {
+          payload: {
+            fullName,
+            nationalID,
+            createdAt: new Date().toISOString(),
+          },
+        };
+      },
+      reducer(state, action) {
+        state.fullName = action.payload.fullName;
+        state.nationalID = action.payload.nationalID;
+        state.createAT = action.payload.createAT;
+      },
+    },
+    updateName(state, action) {
+      state.fullName = action.payload.fullName;
+    },
+  },
+});
+
+export const { createCustomer, updateName } = customerSlice.actions;
+
+export default customerSlice.reducer;
+
+// export default function customerReducer(state = initialStateCostomer, action) {
+//   switch (action.type) {
+//     case "customer/createCustomer":
+//       return {
+//         ...state,
+//         fullName: action.payload.fullName,
+//         nationalID: action.payload.nationalID,
+//         createAT: action.payload.createAT,
+//       };
+//     case "customer/updateName":
+//       return {
+//         ...state,
+//         fullName: action.payload.fullName,
+//       };
+//     default:
+//       return state;
+//   }
+// }
+
+// export function createCustomer(fullName, nationalID) {
+//   return {
+//     type: "customer/createCustomer",
+//     payload: { fullName, nationalID, createAT: new Date().toISOString() },
+//   };
+// }
+
+// export function updateName(fullName) {
+//   return { type: "customer/updateName", payload: fullName };
+// }
